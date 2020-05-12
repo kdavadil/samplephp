3ee636749f29d120a46d06a86103aa8db04af757
Kenneth_Davadilla <ken.davadilla@ubiquilife.com>
2020-05-13 00:29:58 +0800

    add algolia package

--- app/Attendee.php
+++ app/Attendee.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Attendee extends Model
 {
     // public function complete() {
     //     $this->Attendance = true;
     //     $this->save();
-
+    use Searchable;
     // }
 }
